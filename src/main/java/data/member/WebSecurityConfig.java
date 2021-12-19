5bc8f740688af5c459769e34a6caa4dd0a9cddcb
minMin <goals1005@naver.com>
2021-12-19 22:27:06 +0900

    감사함니다!!

--- src/main/java/data/member/WebSecurityConfig.java
+++ src/main/java/data/member/WebSecurityConfig.java
@@ -3,6 +3,7 @@ package data.member;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -16,6 +17,13 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
       return new BCryptPasswordEncoder();
    }
 
+   @Override
+   public void configure(WebSecurity web) throws Exception {
+      web.ignoring().antMatchers(
+          "/thumbnail_image/**"
+      );
+   }
+   
    @Override
    protected void configure(HttpSecurity http) throws Exception {
       http.cors().disable()
